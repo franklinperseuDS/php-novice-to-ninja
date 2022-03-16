088e4038fad4b539f4666851dd466cdd6dddedaa
perseu-at-237228232680 <69854802+franklinperseuDS@users.noreply.github.com>
2022-03-15 21:17:57 -0400

    clean delete

--- public/deletejoke.php
+++ public/deletejoke.php
@@ -1,5 +1,5 @@
 <?php
-if (isset($_POST['id'])){
+
     try{
         $pdo = new PDO('mysql:host=localhost;dbname=ijdb;charset=utf8','ijdbuser','mypassword');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -11,15 +11,8 @@ if (isset($_POST['id'])){
 
         header('location: jokes.php');
      } catch(PDOException $e){
-            $output = ' Database error: '.$e->getMessage() . ' in '. $e->getFile() . ' : ' . $e->getLine();
+        $title = 'An Error has occured in a Delete a Joke';
+        $output = ' Database error: '.$e->getMessage() . ' in '. $e->getFile() . ' : ' . $e->getLine();
     }
-}else {
-    $title = 'Delete a Joke';
 
-    ob_start();
-
-    include __DIR__ .'/../templates/addjokes.html.php';
-
-    $output = ob_get_clean();
-}
-include __DIR__ . '/../templates/layout.html.php';
+   include __DIR__ . '/../templates/layout.html.php';
